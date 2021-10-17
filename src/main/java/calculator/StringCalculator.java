fd61a29ce78f46b3f8ea435aa25d8c1e3f2b60ec
unknown <mujawartabassum117@gmail.com>
2021-10-17 19:23:19 +0530

    create if statement

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,9 +2,24 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-   System.out.println("tab");
-        return 0;
-    }
+   public int add(String input) {
+		// TODO Auto-generated method stub
+		if(input.isEmpty())
+		{
+		return 0;
+		}
+		else if(input.contains(","))
+		{
+			String[] t=input.split(",");
+			return Integer.parseInt(t[0])+Integer.parseInt(t[1]);
+		}
+		else {
+			return Integer.parseInt(input);
+		}
+	}
+	private  int toInt(String input)throws NumberFormatException
+	{
+		return Integer.parseInt(input);
+	}
 
 }
